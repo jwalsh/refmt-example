1fb89d3dcc8bca691f71675fc62d8a943c0e9ec7
Jason Walsh <jwalsh@saylent.com>
2017-10-25 15:40:54 -0400

    Function definition

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const examples = [
   'let    greet    =    fun    name    =>    "Hello "   ^     name;',
   'greet    "world!"    ;     /* "Hello world!" */',
   'let    a   = 1',
-  'let add =fun x y  z  => x  + y + z;',
-  'add a  /* 1 */2   3     /* 6 */ '
+  'let add =fun x y  z  => x  + y + z;', // reduces to the following:
+  'let add x y z => x + y + z;',
+  'add a  /* 1 */2   3     /* 6 */ ',
+  `let greetMore = fun name => {
+  let part1 = "Hello";
+  part1 ^ " " ^ name
+};`,
 ];
 // convert the ocaml syntax to reason syntax
 examples.map(e => {
